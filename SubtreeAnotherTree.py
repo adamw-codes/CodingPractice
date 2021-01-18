8df58ee04ad8148f524d712ba45750b2bed953bd
Adam Wajahat <adamwonline0@yahoo.com>
2021-01-17 19:24:44 -0600

    WIP

---
+++ SubtreeAnotherTree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode):
+        
+
+
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
